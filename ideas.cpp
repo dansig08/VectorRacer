829f9b1e2472631b56d8fe5d1347e1b3d46547aa
dansig08 <dansig08@icloud.com>
2023-11-06 23:40:03 -0500

    added a idea module to test and remember ideas

---
+++ ideas.cpp
@@ -0,0 +1,37 @@
+//This is just where i keep temp ideas for how i am planning on my code running and how i want the console to look while the code is running
+// I'll keep stuff here mostly when i have an idea at the end of a codding session
+
+//console
+//	|---------------------------------------------------------------------------------------------------------------|
+//	|																												|
+//	|																												|
+//	|																												|
+//	|																												|
+//	|				ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo						|
+//	|			oooo																		 oooo					|
+//	|		 ooo 																				 ooo				|
+//	|	   oo 																						oo				|
+//	|	  o																							  o				|
+//	|	 o																							   o 			|
+//	|	o						   ooooooooooooooooooooooooooooooooooooooooooo							o			|
+//	|	o						ooo										  	  ooo						o			|
+//	|	o					  oo											     oo						o			|
+//	|	o					 o													   o					o			|
+//	|	o					o													    o					o			|
+//	|	o                   o													    o					o			|
+//	|	o					o													    o					o			|
+//	|	o					 o												  	   o 					o			|
+//	|	o					  oo											     oo						o			|
+//	|	o				QWE		ooo											  ooo						o			|
+//	|	o				ASD	 	   ooooooooooooooooooooooooooooooooooooooooooo							o			|
+//	|	 o				ZXC							   | |										       o			|
+//	|	  o						*					QWE| |										      o				|
+//	|	   oo										ASD|*|										    oo				|
+//	|		 ooo					   *			ZXC| |									     ooo				|
+//	|			oooo							       | |									 oooo					|
+//	|				ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo						|
+//	|																												|
+//	|																												|
+//	|																												|
+//	|																												|
+//	|_______________________________________________________________________________________________________________|
